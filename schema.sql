d2ce2b68ee10415273b7d50cd7a8988d434e46ea
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-04-25 17:31:22 -0500

    Added seed testing in schema.sql

--- schema.sql
+++ schema.sql
@@ -18,9 +18,21 @@ CREATE TABLE role(
 
 CREATE TABLE employee(
   id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NULL,
+  manager_id INT NULL, -- will reference another employee's id that is deemed their manager
   PRIMARY KEY (id)
 );
+
+INSERT INTO departments (name) 
+VALUES ('IT'), ('HR'), ('Sales'), ('Marketing');
+
+SELECT * FROM departments;
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Senior Digital Marketer', '90000', 4); -- Will need a conditional to insert the corresponding dept_id based on the dept Name someone selects in the terminal
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Senior Digital Marketer', '90000', 4);
